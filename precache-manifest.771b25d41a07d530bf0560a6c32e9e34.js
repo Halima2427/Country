5065392b3c07be67759333f664798bd7ff6a41a0
Halima2427 <halimafarz@gmail.com>
2019-11-05 17:13:42 +0530

    Updates

---
+++ precache-manifest.771b25d41a07d530bf0560a6c32e9e34.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6d0c16a55f5cf059e1d",
+    "url": "/Halima2427/Country/static/js/main.a6d0c16a.chunk.js"
+  },
+  {
+    "revision": "64969d4ccd8e54913c32",
+    "url": "/Halima2427/Country/static/js/1.64969d4c.chunk.js"
+  },
+  {
+    "revision": "72c6f8ba5c52d19bb220",
+    "url": "/Halima2427/Country/static/js/runtime~main.72c6f8ba.js"
+  },
+  {
+    "revision": "e462198c49d0224a89f11e5d575397da",
+    "url": "/Halima2427/Country/index.html"
+  }
+];
